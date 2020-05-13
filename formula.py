33561c43a84845d1c385df8b6f10d509e2c648e8
EduardLimonov <49101914+EduardLimonov@users.noreply.github.com>
2020-05-13 23:11:30 +0300

    Add files via upload

--- formula.py
+++ formula.py
@@ -7,7 +7,8 @@
 from __future__ import annotations
 from typing import *
 from ElemBlock import ElemBlock
-from pylatex import Math
+from pylatex import Math, NoEscape
+from position import Position
 
 
 FRAC = '\\frac'
@@ -22,8 +23,9 @@ class Formula:
     LOW = 'low'
     HAS_LIMITS = {'\\frac', '\\int', '\\sum'}
     INF = 9999
-    CENTER_DY = 0.15
+    CENTER_DY = 0.65
     NOSYMB = {'\\frac'}
+    LIMIT_DY = 0.6
 
 
     def __init__(self, elemBlocks: List[ElemBlock]):
@@ -33,12 +35,12 @@ class Formula:
         self.pos = elemBlocks[0].getPos()
 
     def recognizeLimits(self, elemBlocks: List[ElemBlock]) \
-            -> Tuple[List[Tuple[ElemBlock, Dict[Union[Formula.TOP, Formula.LOW], List[ElemBlock]]]], List[ElemBlock]]:
+            -> Tuple[List[Tuple[ElemBlock, Dict[Union[Formula.TOP, Formula.LOW], ElemBlock]]], List[ElemBlock]]:
         # данный метод выделяет все подформулы, которые являются пределами; возвращает список кортежей:
         # блок-родитель -- словарь с пределами;
         # а также список оставшихся elemBlock'ов
 
-        done = set()
+        done = []
         res = []
 
         for block in elemBlocks:
@@ -46,13 +48,36 @@ class Formula:
                 topBlocks = Formula.findBlocks(block, elemBlocks, Formula.TOP)
                 lowBlocks = Formula.findBlocks(block, elemBlocks, Formula.LOW)
 
+                topLimit = Formula.uniteBlocks(topBlocks)
+                lowLimit = Formula.uniteBlocks(lowBlocks)
+
                 res.append(
-                    (block, {Formula.TOP : topBlocks, Formula.LOW: lowBlocks})
+                    (block, {Formula.TOP : topLimit, Formula.LOW: lowLimit})
                 )
 
-                [done.add(block) for block in (topBlocks + lowBlocks)]
+                [done.append(block) for block in (topBlocks + lowBlocks)]
 
         return res, [block for block in elemBlocks if block not in done]
+
+    @staticmethod
+    def uniteBlocks(blocks) -> ElemBlock:
+        # метод создает tex-код для данной формулы
+        Formula.sortBlocks(blocks)
+        content = ''
+        yLine = blocks[0].getPos().center().y
+        position = blocks[0].getPos()
+
+        for block in blocks:
+            position = position.bounding_rect(block.getPos())
+            direct = Formula.findDirection(yLine, block)
+            if direct == 0:
+                yLine = block.getPos().center().y
+
+            content += f'{["","^","_"][direct]}{block.getOutput()}' # 0 - empty; 1 - ^; -1 - _
+
+        content = "{" + content + "}"
+
+        return ElemBlock(content, position)
 
     @staticmethod
     def hasLimits(block: ElemBlock) -> bool:
@@ -62,7 +87,7 @@ class Formula:
     @staticmethod
     def findBlocks(block: ElemBlock, elemBlocks: List[ElemBlock], side: Union[Formula.TOP, Formula.LOW]) \
              -> List[ElemBlock]:
-        # возвращает список блоков из elemBlocks, находящихся в стороне side о block
+        # возвращает список блоков из elemBlocks, находящихся в стороне side от block
 
         def findBorders(block: ElemBlock, elemBlocks: List[ElemBlock]) -> Tuple[int, int]:
             # функция находит левую и правую границы зоны, отводимой для записи пределов block
@@ -83,13 +108,15 @@ class Formula:
         res = []
         if side == Formula.TOP:
             yLine = block.getPos().top()
+            sign = -1
         else:
             yLine = block.getPos().bottom()
+            sign = 1
 
         bl, br = findBorders(block, elemBlocks)
         for b in elemBlocks:
             if b != block and bl < b.getPos().left() and br > b.getPos().right():
-                if Formula.inDirection(yLine, b, side):
+                if Formula.inDirection(yLine + sign * Formula.LIMIT_DY * b.getPos().h, b, side):
                     res.append(b)
 
         return res
@@ -101,12 +128,12 @@ class Formula:
 
     @staticmethod
     def inDirection(yLine: int, block: ElemBlock, side: Union[Formula.TOP, Formula.LOW]) -> bool:
-        return side == Formula.TOP and block.getPos().top() < yLine or block.getPos().bottom() > yLine
+        return side == Formula.TOP and block.getPos().top() < yLine or block.getPos().bottom() > yLine and side == Formula.LOW
 
     @staticmethod
     def sortBlocks(elemBlocks: List[ElemBlock]) -> None:
         # сортирует список блоков (в порядке возрастания горизонтальной координаты)
-        elemBlocks.sort(lambda _: _.getPos().center().x)
+        elemBlocks.sort(key=lambda _: _.getPos().center().x)
 
     @staticmethod
     def findLimits(elemBlock: ElemBlock, limits: List) -> Dict:
@@ -125,40 +152,37 @@ class Formula:
         y2 = second.getPos().center().y
         if Formula.onLine(yLine, second):
             return 0
-        elif yLine < y2:
+        elif yLine > y2:
             return 1
         else:
             return -1
 
-    def recognize_structure(self, elemBlocks: List[ElemBlock], limits: List[Tuple[ElemBlock, Dict]]) -> None:
+    def recognize_structure(self, elemBlocks: List[ElemBlock], limits: List[Tuple[ElemBlock, Dict[Union, ElemBlock]]]) -> None:
         # метод создает tex-код для данной формулы
         Formula.sortBlocks(elemBlocks)
         self.texCode = ''
         yLine = elemBlocks[0].getPos().center().y
 
         for block in elemBlocks:
+            direct = Formula.findDirection(yLine, block)
+            if direct == 0:
+                yLine = block.getPos().center().y
 
             # добавление пределов для block (если они есть)
             if Formula.hasLimits(block):
                 self.texCode += block.getOutput()
                 lims = Formula.findLimits(block, limits)
 
-                s = '_' * (symb := not Formula.noSymb(block.getOutput()))
+                symb = not Formula.noSymb(block.getOutput())
 
 
-                for lowBlock in lims[Formula.LOW]:
-                    s += lowBlock.getOutput()
-                self.texCode += s
+                self.texCode += '^' * symb + lims[Formula.TOP].getOutput()
 
-                s = '^' * symb
+                self.texCode += '_' * symb + lims[Formula.LOW].getOutput()
 
-                for topBlock in lims[Formula.TOP]:
-                    s += topBlock.getOutput()
-                self.texCode += s
+            else:
+                self.texCode += f'{["","^","_"][direct]}{block.getOutput()}' # 0 - empty; 1 - ^; -1 - _
 
-            direct = Formula.findDirection(yLine, block)
-            self.texCode += f'{["","^","_"][direct]}{block.getOutput()}' # 0 - empty; 1 - ^; -1 - _
-            yLine = block.getPos().center().y
 
     def getFormula(self) -> ElemBlock(Math, Position) :
         '''
